90af376af5ea0447bce368c942503bd20df85188
unknown <555981@PC237742.cts.com>
2016-06-13 15:31:30 +0530

    demohealthcare

---
+++ app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '0a3efa480a48295e767efd6112bc6d8e',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'www_sampledata1_com',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'full_page' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 07 Jun 2016 07:55:15 +0000',
+  ),
+);
